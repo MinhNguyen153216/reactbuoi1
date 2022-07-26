57156f04a59c056d89ee66032a0a4c6b8a89672a
MinhNguyen153216 <minhnguyen153216@gmail.com>
2022-07-26 21:05:31 +0700

    second commit

--- src/BaiTapLayoutComponent/Header.jsx
+++ src/BaiTapLayoutComponent/Header.jsx
@@ -6,7 +6,7 @@ export default class Header extends Component {
       <div className="bg-dark">
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark container">
           <a className="navbar-brand" href="#">
-            Navbar
+            Start Bootstrap
           </a>
           <button
             className="navbar-toggler d-lg-none"
@@ -17,52 +17,32 @@ export default class Header extends Component {
             aria-expanded="false"
             aria-label="Toggle navigation"
           />
-          <div className="collapse navbar-collapse" id="collapsibleNavId">
-            <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="collapsibleNavId"
+          >
+            <ul className="navbar-nav mt-2 mt-lg-0">
               <li className="nav-item active">
-                <a className="nav-link" href="#">
+                <a className="nav-link text-white" href="#">
                   Home <span className="visually-hidden">(current)</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Link
+                  About
                 </a>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="dropdownId"
-                  data-bs-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Dropdown
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Services
                 </a>
-                <div className="dropdown-menu" aria-labelledby="dropdownId">
-                  <a className="dropdown-item" href="#">
-                    Action 1
-                  </a>
-                  <a className="dropdown-item" href="#">
-                    Action 2
-                  </a>
-                </div>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
               </li>
             </ul>
-            <form className="d-flex my-2 my-lg-0">
-              <input
-                className="form-control me-sm-2"
-                type="text"
-                placeholder="Search"
-              />
-              <button
-                className="btn btn-outline-success my-2 my-sm-0"
-                type="submit"
-              >
-                Search
-              </button>
-            </form>
           </div>
         </nav>
       </div>
