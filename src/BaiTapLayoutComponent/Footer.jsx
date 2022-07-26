c477cbb5b04e730d2a732b40f98bd1ab879ba7a6
MinhNguyen153216 <minhnguyen153216@gmail.com>
2022-07-26 21:51:34 +0700

    completed

--- src/BaiTapLayoutComponent/Footer.jsx
+++ src/BaiTapLayoutComponent/Footer.jsx
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
-    )
+      <div className="bg-dark">
+        <div className="py-5 container">
+          <p className="m-0 text-center text-white">
+            Copyright © Your Website 2022
+          </p>
+        </div>
+      </div>
+    );
   }
 }
--- src/BaiTapLayoutComponent/Item.jsx
+++ src/BaiTapLayoutComponent/Item.jsx
@@ -1,9 +1,134 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Item extends Component {
   render() {
     return (
-      <div>Item</div>
-    )
+      <div className="pt-4">
+        <div className="container px-lg-5">
+          <div className="row">
+            <div className="col-4 col-lg-6 col-xxl-4 mb-5">
+              <div className="card " style={{ width: "18rem", margin: "auto" }}>
+                <img
+                  src="https://picsum.photos/200/200"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Expedita fugit harum nisi facere dolorem deserunt numquam
+                    vel repellendus dolor non.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Find Out More!
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 col-lg-6 col-xxl-4 mb-5">
+              <div className="card" style={{ width: "18rem", margin: "auto" }}>
+                <img
+                  src="https://picsum.photos/200/200"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Expedita fugit harum nisi facere dolorem deserunt numquam
+                    vel repellendus dolor non.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Find Out More!
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 col-lg-6 col-xxl-4 mb-5">
+              <div className="card" style={{ width: "18rem", margin: "auto" }}>
+                <img
+                  src="https://picsum.photos/200/200"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Expedita fugit harum nisi facere dolorem deserunt numquam
+                    vel repellendus dolor non.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Find Out More!
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 col-lg-6 col-xxl-4 mb-5">
+              <div className="card" style={{ width: "18rem", margin: "auto" }}>
+                <img
+                  src="https://picsum.photos/200/200"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Expedita fugit harum nisi facere dolorem deserunt numquam
+                    vel repellendus dolor non.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Find Out More!
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 col-lg-6 col-xxl-4 mb-5">
+              <div className="card" style={{ width: "18rem", margin: "auto" }}>
+                <img
+                  src="https://picsum.photos/200/200"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Expedita fugit harum nisi facere dolorem deserunt numquam
+                    vel repellendus dolor non.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Find Out More!
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 col-lg-6 col-xxl-4 mb-5">
+              <div className="card" style={{ width: "18rem", margin: "auto" }}>
+                <img
+                  src="https://picsum.photos/200/200"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Expedita fugit harum nisi facere dolorem deserunt numquam
+                    vel repellendus dolor non.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Find Out More!
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
